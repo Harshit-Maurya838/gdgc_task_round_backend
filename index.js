465cacdfc7c68c446052d535bf79f87fa4be3b88
Harshit-Maurya <44harshitmaurya@gmail.com>
2024-11-24 14:22:33 +0530

    Port problem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 5050;
+const port = process.env.PORT || 5050;
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: true }));
